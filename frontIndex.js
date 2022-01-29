51361abfc628daf48c0e2d95be07d6e4d7be12c2
shivkar2n <shivkar2n@gmail.com>
2022-01-29 20:47:38 +0530

    Basic schema and api

--- frontIndex.js
+++ frontIndex.js
@@ -6,5 +6,132 @@ const sessions = require("express-session");
 const cookieParser = require("cookie-parser");
 const ejs = require("ejs");
 
+const prisma = new prim.PrismaClient();
+const app = express();
+
+const oneDay = 1000 * 60 * 60 * 24;
+
+// {{{MIDDLEWARE
+auth = (req, res, next) => {
+  try {
+    console.log(req.session);
+    if (!req.session.userid) {
+      res.status(403).send({ message: "Not logged in!" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+  }
+};
+
+app.use(
+  sessions({
+    secret: "process.env.SECRET_KEY",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false,
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  //console.log("Time:", Date.now());
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "pages")));
+app.set("view engine", "ejs");
+// }}}
+
+//{{{LOGIN PAGE
+app.all("/login", async (req, res) => {
+  try {
+    if (req.session.userid) {
+      //return res.redirect("/");
+      console.log(req.session);
+      return res.status(200).send({ message: "Already logged in" });
+    }
+
+    if (req.method == "POST") {
+      const frontData = req.body;
+
+      user = await prisma.user.findUnique({
+        where: {
+          email: frontData.email,
+        },
+      });
+
+      if (user != null) {
+        if (user.password != frontData.password) {
+          return res.status(404).send({ message: "Incorrect email/password" });
+        }
+      } else {
+        return res.status(404).send({ message: "Incorrect email/password" });
+      }
+
+      const employee = await prisma.employee.findFirst({
+        where: {
+          user: {
+            email: frontData.email,
+          },
+        },
+      });
+
+      session = req.session;
+      session.userid = frontData.email;
+      session.eid = employee.id;
+
+      return res.status(200).send({ message: "Success" });
+      //return res.status(200).redirect("/");
+    }
+
+    return res.status(200).sendFile(path.join(__dirname, "/pages/login.html"));
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: err });
+  }
+});
+//}}}
+
+//{{{LOGOUT
+app.get("/logout", (req, res) => {
+  try {
+    req.session.destroy();
+    console.log(req.session);
+    return res.status(200).redirect("/login");
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: "Logout Error!" });
+  }
+});
+//}}}
+
+//{{{REGISTER PAGE
+app.get("/register", (req, res) => {
+  try {
+    if (req.session.userid) {
+      return res.status(200).send({ message: "Already logged in" });
+      //return res.redirect("/");
+    }
+    return res
+      .status(200)
+      .sendFile(path.join(__dirname, "/pages/register.html"));
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: err });
+  }
+});
+//}}}
+
 const port = process.env.PORT2 || 3001;
 app.listen(port, () => console.log(`listening on port ${port}`));
--- index.js
+++ index.js
@@ -5,5 +5,137 @@ const bodyParser = require("body-parser");
 const prisma = new prim.PrismaClient();
 const app = express();
 
+// {{{ MIDDLEWARE
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  //console.log("Time:", Date.now());
+  next();
+});
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(cors({ origin: "*" }));
+// }}}
+
+//{{{REGISTER
+app.post("/api/register", async (req, res) => {
+  try {
+    const frontData = req.body;
+    const user = await prisma.user.create({
+      data: {
+        username: frontData.username,
+        email: frontData.email,
+        password: frontData.password,
+      },
+    });
+    res.status(200).send({ message: "Register Successful!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: err });
+  }
+});
+//}}}
+
+//{{{VIEW PROJECTS
+app.get("/api/listProjects", async (req, res) => {
+  try {
+    const projects = await prisma.project.findMany();
+    console.log(projects);
+    res
+      .status(200)
+      .send({ data: projects, message: "Projects list request successful!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: err });
+  }
+});
+//}}}
+
+//{{{CREATE BUGS
+app.post("/api/createBugs/:id", async (req, res) => {
+  try {
+    const frontData = req.body;
+    const bug = await prisma.bug.create({
+      data: {
+        raisedBy: frontData.raisedBy,
+        description: frontData.description,
+        threatLevel: frontData.threatLevel,
+        deadline: frontData.deadline,
+        resolved: frontData.resolved,
+        projectId: parseInt(req.params.id),
+      },
+    });
+    console.log(bug);
+    res.status(200).send({ message: "Comments successfully created!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: err });
+  }
+});
+//}}}
+
+//{{{VIEW BUGS
+app.get("/api/listBugs/:id", async (req, res) => {
+  try {
+    const bugs = await prisma.bug.findMany({
+      where: {
+        projectId: parseInt(req.params.id),
+      },
+    });
+    console.log(bugs);
+    res
+      .status(200)
+      .send({ data: bugs, message: "Projects list request successful!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: err });
+  }
+});
+//}}}
+
+//{{{CREATE COMMENTS
+app.post("/api/createComment/:id", async (req, res) => {
+  try {
+    const frontData = req.body;
+    const comments = await prisma.comment.create({
+      data: {
+        body: frontData.body,
+        bugId: parseInt(req.params.id),
+        commenterId: frontData.commenterId,
+      },
+    });
+    console.log(comments);
+    res.status(200).send({ message: "Comments successfully created!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: err });
+  }
+});
+//}}}
+
+//{{{VIEW COMMENTS
+app.get("/api/listComments/:id", async (req, res) => {
+  try {
+    const comments = await prisma.comment.findMany({
+      where: {
+        bugId: parseInt(req.params.id),
+      },
+    });
+    console.log(comments);
+    res
+      .status(200)
+      .send({ data: comments, message: "Comments list request successful!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: err });
+  }
+});
+//}}}
+
 const port = process.env.PORT1 || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
